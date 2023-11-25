39f76ec53fc67c80f7ae666dab145a89526cb572
ml-mahendra <ml.mahendranath@gmail.com>
2023-11-25 13:17:56 +0000

    Day-4

--- Guess the number game.py
+++ Guess the number game.py
@@ -4,32 +4,34 @@ import random
 
 print("Hello There..!")
 name = input("What's your name?")
+secretNumber = random.randint(1,77)
+
 
 if name != "":
-	print("Well", name , ", Are you ready to paly Guess my Number game?")
-
-
-YN = input("Yes or No")
-
-if (YN == 'Y' or 'y' or 'Yes' or 'YES' or 'yes'):
-	gameplay
+    print("well, "+name+ " are you ready to play my game (Guess my Number)?")
+    Yn = input("Enter your ans (Yes/No)?")
+    if (Yn == 'Y' or 'y' or 'Yes' or 'YES' or 'yes'):
+        print("Game starts, I am thinking of a number from 1 to 77, And it says Guess me...")
+        
+        for guesses in range(1,7):
+            print("Take a guess, Remember you have only 6 guesses...")
+            guess = int(input("Enter your guess:"))
+            
+            if guess < secretNumber:
+                print("Your guess is low")
+            elif guess > secretNumber:
+                print("Your guess is high")
+            else:
+                break
+        
+        if guess == secretNumber:
+            print("Woha, "+name+" You guessed my number in limited chances...!")
+        else:
+            print("Nope, the number i was thinkng was:" + secretNumber + "Why don't you try another time???")
+        
+    else:
+        print("Ohh... No, Sad to see you go..!")
 else:
-	print("Ohh..! No. Sad to see you go.")
+    print("Enter a name to proceed...")
+    
 
-
-
-def gameplay(secretNumber):
-	for guesses in range (1,7): 
-		print("Take a Guess, Remember you will get only 6 Guess.")
-		guess = int(input("Enter your Guess number:"))
-		if guess < secretNumber:
-			print("Your guess is low.")
-		elif guess >secretNumber:
-			print("Your guess is high.")
-		else:
-			break
-
-if guess == secretNumber:
-	print("Woha, "+name+" You guessed my number in limited chances...!")
-else:
-	print("Nope, the number i was thinkng was:" + secretNumber + "Why don't you try another time???")
